b1f2987f84e5cf737aec0e77b8372964ab04a759
alexander-daniel <alex@lesinstruments.com>
2014-06-09 14:37:22 -0400

    get figure and get image examples

--- examples/get_figure_and_save.js
+++ examples/get_figure_and_save.js
@@ -2,5 +2,6 @@ var plotly = require('../.')('username','api_key');
 
 plotly.get_figure('file_owner', 'file_id', function (err, figure) {
 	if (err) console.log(err);
-	console.log(figure);
+	// now save that figure as a static image!
+	plotly.save_image(figure, 'path/to/image_name');
 });
---
+++ examples/save_image.js
@@ -0,0 +1,17 @@
+var plotly = require('../.')('username','api_key');
+
+var trace1 = {
+  x: [1, 2, 3, 4], 
+  y: [10, 15, 13, 17], 
+  type: "scatter"
+};
+
+var trace2 = {
+  x: [1, 2, 3, 4], 
+  y: [16, 5, 11, 9], 
+  type: "scatter"
+};
+
+var data = [trace1, trace2];
+
+plotly.save_image({'data': data}, 'path/to/image_name');
