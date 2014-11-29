68d09cb9628f51cda35cec5f25f6980b3fc1774d
Simon Nattress <nattress@gmail.com>
2014-11-29 10:47:04 -0800

    Fix broken callback in saveImage
    saveImage only calls the callback method under error conditions.  Fixed
    so that it also calls it when saveImage successfully writes the image
    file to disk.
    
    Callback was not being checked for presence, so if it wasn't passed in
    an exception would be thrown. Fixed to provide a dummy callback if the
    caller didn't specify one.
    
    Removed spurious console logging on successful image write and fixed
    some formatting in writeFile.

--- index.js
+++ index.js
@@ -176,6 +176,8 @@ Plotly.prototype.getFigure = function (fileOwner, fileId, callback) {
 
 Plotly.prototype.saveImage = function (figure, path, callback) {
   var self = this;
+  callback = callback || function() {};
+
   figure = JSON.stringify(figure);
 
   var headers = {
@@ -206,8 +208,7 @@ Plotly.prototype.saveImage = function (figure, path, callback) {
         } else {
           var image = JSON.parse(body).payload;
           writeFile(path, image, function (err) {
-            if (err) callback(err);
-            console.log('image saved!');
+            callback(err);
           })
         }
       });
@@ -222,10 +223,11 @@ Plotly.prototype.saveImage = function (figure, path, callback) {
 // helper fn to create folders if they don't exist in the path
 function writeFile (path, image, callback) {
   mkdirp(getDirName(path), function (err) {
-    if (err) return callback(err)
-      fs.writeFile(path + '.png', image, 'base64', function () {
-        callback(null);
-      })
+    if (err)
+        callback(err);
+    fs.writeFile(path + '.png', image, 'base64', function () {
+      callback(null);
+    });
   });
 }
 
