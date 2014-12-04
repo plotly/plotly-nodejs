9bf8f030c5080a09c0afc12e4a510794011c03b8
Alejandro <alex.vados@gmail.com>
2014-12-04 13:06:51 -0500

    cleanup

--- examples/streaming-example.js
+++ examples/streaming-example.js
@@ -1,28 +1,26 @@
-var plotly = require('../.')('your_username','your_apikey');
+'use strict';
 
-var initdata = [{x:[], y:[], stream:{token:'your_streamtoken', maxpoints:200}}];
-var initlayout = {fileopt : "extend", filename : "nodenodenode"};
+var plotly = require('../.')('plotlyUsername','apiKey');
+
+var initdata = [{x:[], y:[], stream:{token:'streamToken', maxpoints:200}}];
+var initlayout = {fileopt : 'overwrite', filename : 'nodenodenode'};
 
 plotly.plot(initdata, initlayout, function (err, msg) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(msg);
+    if (err) return console.log(err);
+    console.log(msg);
 
-	var stream1 = plotly.stream('your_streamtoken', function (err, res) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(res);
-    }
-		clearInterval(loop); // once stream is closed, stop writing
-	});
+    var stream1 = plotly.stream('streamToken', function (err, res) {
+        if (err) return console.log(err);
+        console.log(res);
+        clearInterval(loop); // once stream is closed, stop writing
+    });
 
-	var i = 0;
-	var loop = setInterval(function () {
-			var streamObject = JSON.stringify({ x : i, y : i });
-			stream1.write(streamObject+'\n');
-			i++;
-	}, 1000);
+    var i = 0;
+    var loop = setInterval(function () {
+        var data = { x : i, y : i * (Math.random() * 10) };
+        var streamObject = JSON.stringify(data);
+        stream1.write(streamObject+'\n');
+        i++;
+    }, 1000);
 });
 
--- index.js
+++ index.js
@@ -1,259 +1,257 @@
+'use strict';
+
 var https = require('https');
 var http = require('http');
 var jsonStatus = require('./statusmsgs.json');
 var url = require('url');
 var fs = require('fs');
-var mkdirp = require("mkdirp");
-var getDirName = require("path").dirname;
+var mkdirp = require('mkdirp');
+var getDirName = require('path').dirname;
 
 module.exports = Plotly;
 
 function Plotly(username,apiKey) {
-  if (!(this instanceof Plotly))  {
-    return new Plotly(username,apiKey);
-  }
+    if (!(this instanceof Plotly)) return new Plotly(username,apiKey);
 
-  var opts = {};
+    var opts = {};
 
-  if (typeof username === 'object') {
-    opts = username;
-    this.username = opts.username;
-    this.apiKey = opts.apiKey;
-    this.host = opts.host || 'plot.ly';
-    this.port = opts.port || 443;
-  } else {
-    this.username = username;
-    this.apiKey = apiKey;
-    this.host = 'plot.ly';
-    this.port = 443;
-  }
-  this.streamHost = '';
-  this.version="0.0.2";
-  this.platform="nodejs";
-  this.origin="plot";
+    if (typeof username === 'object') {
+        opts = username;
+        this.username = opts.username;
+        this.apiKey = opts.apiKey;
+        this.host = opts.host || 'plot.ly';
+        this.port = opts.port || 443;
+    } else {
+        this.username = username;
+        this.apiKey = apiKey;
+        this.host = 'plot.ly';
+        this.port = 443;
+    }
+        this.streamHost = '';
+        this.version='0.0.2';
+        this.platform='nodejs';
+        this.origin='plot';
 }
 
 Plotly.prototype.plot = function(data, graphOptions, callback) {
-  var self = this;
-  if (!callback) { callback = function() {}; }
+    var self = this;
+    if (!callback) { callback = function() {}; }
 
-  // allow users to just pass in an object for the data, data = {x:[],y:[]}
-  if (!Array.isArray(data)) data = [data];
+    // allow users to just pass in an object for the data, data = {x:[],y:[]}
+    if (!Array.isArray(data)) data = [data];
 
-  var urlencoded = '';
-  var pack = {
-    'platform': self.platform,
-    'version': self.version,
-    'args': JSON.stringify(data),
-    'kwargs': JSON.stringify(graphOptions),
-    'un': self.username,
-    'key': self.apiKey,
-    'origin': self.origin
-  };
+    var urlencoded = '';
+    var pack = {
+        'platform': self.platform,
+        'version': self.version,
+        'args': JSON.stringify(data),
+        'kwargs': JSON.stringify(graphOptions),
+        'un': self.username,
+        'key': self.apiKey,
+        'origin': self.origin
+    };
 
-  for (var key in pack) {
-    urlencoded += key + "=" + pack[key] + "&";
-  }
+    for (var key in pack) {
+        urlencoded += key + '=' + pack[key] + '&';
+    }
 
-  // trim off last ambersand
-  urlencoded = urlencoded.substring(0, urlencoded.length - 1);
+    // trim off last ambersand
+    urlencoded = urlencoded.substring(0, urlencoded.length - 1);
 
-  var options = {
-    host: self.host,
-    port: self.port,
-    path: '/clientresp',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': urlencoded.length
-    },
-    withCredentials: false
-  };
+    var options = {
+        host: self.host,
+        port: self.port,
+        path: '/clientresp',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': urlencoded.length
+        },
+        withCredentials: false
+    };
 
-  var req = https.request(options, function (res) {
-    parseRes(res, function (err, body) {
-      body = JSON.parse(body);
-      if ( body['stream-status'] != undefined) {
-        self.streamHost = url.parse(body['stream-host']).hostname;
-      }
-      if ( body.error.length > 0 ) {
-        var error = new Error(body.error);
-        error.body = body;
-        error.statusCode = res.statusCode;
-        callback(error);
-      } else {
-        callback(null, {
-          streamstatus : body['stream-status'],
-          url: body.url,
-          message: body.message,
-          warning: body.warning,
-          filename: body.filename,
-          error: body.error
+    var req = https.request(options, function (res) {
+        parseRes(res, function (err, body) {
+            body = JSON.parse(body);
+
+            if ( body['stream-status'] != undefined) {
+                self.streamHost = url.parse(body['stream-host']).hostname;
+            }
+
+            if ( body.error.length > 0 ) {
+                var error = new Error(body.error);
+                error.body = body;
+                error.statusCode = res.statusCode;
+                callback(error);
+            } else {
+                callback(null, {
+                    streamstatus : body['stream-status'],
+                    url: body.url,
+                    message: body.message,
+                    warning: body.warning,
+                    filename: body.filename,
+                    error: body.error
+                });
+            }
         });
-      }
-      
     });
-  });
 
-  req.on('error', function(err) {
-    callback(err);
-  });
+    req.on('error', function(err) {
+        callback(err);
+    });
 
-  req.write(urlencoded);
-  req.end();
+    req.write(urlencoded);
+    req.end();
 };
 
 Plotly.prototype.stream = function(token, callback) {
-  var self = this;
-  var opts = {};
+    var self = this;
+    var opts = {};
 
-  if (typeof token === 'object') {
-    opts = token;
-    token = opts.token;
-    var host = opts.streamHost || self.streamHost;
-    var port = opts.port || 80;
-  }
-
-  var options = {
-    host: host || self.streamHost || 'stream.plot.ly',
-    port: port || 80,
-    path: '/',
-    method: 'POST',
-    agent: false,
-    headers: { "plotly-streamtoken" : token }
-  };
-
-  if (!callback) { callback = function() {}; }
-
-  var stream = http.request(options, function(res) {
-    var message = jsonStatus[res.statusCode];
-    if (res.statusCode !== 200) {
-      var error = new Error(message);
-      error.statusCode = res.statusCode;
-      callback(error);
-    } else {
-      callback(null, {msg : message, statusCode: res.statusCode});
+    if (typeof token === 'object') {
+        opts = token;
+        token = opts.token;
+        var host = opts.streamHost || self.streamHost;
+        var port = opts.port || 80;
     }
-  });
 
-  if (stream.setTimeout) stream.setTimeout(Math.pow(2, 32) * 1000);
-  return stream;
+    var options = {
+        host: host || self.streamHost || 'stream.plot.ly',
+        port: port || 80,
+        path: '/',
+        method: 'POST',
+        agent: false,
+        headers: { 'plotly-streamtoken' : token }
+    };
+
+    if (!callback) { callback = function() {}; }
+
+    var stream = http.request(options, function(res) {
+        var message = jsonStatus[res.statusCode];
+        
+        if (res.statusCode !== 200) {
+            var error = new Error(message);
+            error.statusCode = res.statusCode;
+            callback(error);
+        } else {
+            callback(null, {msg : message, statusCode: res.statusCode});
+        }
+    });
+
+    if (stream.setTimeout) stream.setTimeout(Math.pow(2, 32) * 1000);
+    return stream;
 };
 
 
 Plotly.prototype.getFigure = function (fileOwner, fileId, callback) {
-  var self = this;
+    var self = this;
 
-  if (!callback) { callback = function() {}; }
+    if (!callback) { callback = function() {}; }
 
-  var headers = {
-    'plotly-username': this.username,
-    'plotly-apikey': this.apiKey,
-    'plotly-version': this.version,
-    'plotly-platform': this.platform
-  };
+    var headers = {
+        'plotly-username': this.username,
+        'plotly-apikey': this.apiKey,
+        'plotly-version': this.version,
+        'plotly-platform': this.platform
+    };
 
-  var resource = '/apigetfile/'+fileOwner+'/'+fileId;
+    var resource = '/apigetfile/'+fileOwner+'/'+fileId;
 
-  var options = {
-    host: self.host,
-    path: resource,
-    port: self.port,
-    headers: headers,
-    method: 'GET'
-  };
+    var options = {
+        host: self.host,
+        path: resource,
+        port: self.port,
+        headers: headers,
+        method: 'GET'
+    };
 
-  var req = https.get(options, function (res) {
-    parseRes(res, function (err, body) {
-      if (JSON.parse(body).error) {
-        var err = JSON.parse(body).error;
-        callback(err);
-      } else {
-        var figure = JSON.parse(body).payload.figure;
-        callback(null, figure);
-      }
-      
-    })
-  });
+    var req = https.get(options, function (res) {
+        parseRes(res, function (err, body) {
+            if (JSON.parse(body).error) {
+                err = JSON.parse(body).error;
+                callback(err);
+            } else {
+                var figure = JSON.parse(body).payload.figure;
+                callback(null, figure);
+            }
+        });
+    });
 
-  req.end();
-}
+    req.end();
+};
 
 Plotly.prototype.saveImage = function (figure, path, callback) {
-  var self = this;
-  callback = callback || function() {};
+    var self = this;
+    callback = callback || function() {};
 
-  figure = JSON.stringify(figure);
+    figure = JSON.stringify(figure);
 
-  var headers = {
-    'plotly-username': self.username,
-    'plotly-apikey': self.apiKey,
-    'plotly-version': self.version,
-    'plotly-platform': self.platform,
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': figure.length
-  };
+    var headers = {
+        'plotly-username': self.username,
+        'plotly-apikey': self.apiKey,
+        'plotly-version': self.version,
+        'plotly-platform': self.platform,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': figure.length
+    };
 
-  var options = {
-    hostname: self.host,
-    path : '/apigenimage/',
-    port: self.port,
-    headers: headers,
-    method: 'POST',
-    agent: false
-  };
+    var options = {
+        hostname: self.host,
+        path : '/apigenimage/',
+        port: self.port,
+        headers: headers,
+        method: 'POST',
+        agent: false
+    };
 
-  var req = https.request(options, function (res) {
-    if (res.statusCode !== 200) {
-      var error = new Error('Bad response status code ' + res.statusCode);
-      callback(error);
-    } else {
-      parseRes(res, function (err, body) {
-        if (err) {
-          callback(err);
+    var req = https.request(options, function (res) {
+        if (res.statusCode !== 200) {
+            var error = new Error('Bad response status code ' + res.statusCode);
+            callback(error);
         } else {
-          var image = JSON.parse(body).payload;
-          writeFile(path, image, callback);
+            parseRes(res, function (err, body) {
+                if (err) {
+                    callback(err);
+                } else {
+                    var image = JSON.parse(body).payload;
+                    writeFile(path, image, callback);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 
-  req.write(figure);
-  req.end();
-}
+    req.write(figure);
+    req.end();
+};
 
 
 // helper fn to create folders if they don't exist in the path
 function writeFile (path, image, callback) {
-  mkdirp(getDirName(path), function (err) {
-    if (err)
-        callback(err);
-    fs.writeFile(path + '.png', image, 'base64', callback);
-  });
+    mkdirp(getDirName(path), function (err) {
+        if (err) return callback(err);
+        fs.writeFile(path + '.png', image, 'base64', callback);
+    });
 }
 
 
 // response parse helper fn
 function parseRes (res, cb) {
-  var body = '';
-  if ('setEncoding' in res) {
-    res.setEncoding('utf-8');
-  }
-  res.on('data', function (data) {
-    //console.log(data);
-    body += data;
-    if (body.length > 1e10) {
-      // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQ
-      res.connection.destroy();
-      res.writeHead(413, {'Content-Type': 'text/plain'});
-      res.end("req body too large");
-      return cb(new Error("body overflow"));
-    }
-  });
+    var body = '';
+    if ('setEncoding' in res) res.setEncoding('utf-8');
+    
+    res.on('data', function (data) {
+        body += data;
+        if (body.length > 1e10) {
+            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQ
+            res.connection.destroy();
+            res.writeHead(413, {'Content-Type': 'text/plain'});
+            res.end('req body too large');
+            return cb(new Error('body overflow'));
+        }
+    });
 
-  res.on('end', function () {
-    cb(null, body);
-  });
+    res.on('end', function () {
+        cb(null, body);
+    });
 
 }
